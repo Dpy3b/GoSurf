058ba12ce4d685d072d80d9d3dae9dc3d57b98be
oTBe4aeT-Dpy3b <57526052+oTBe4aeT-Dpy3b@users.noreply.github.com>
2020-05-17 19:56:39 +0300

    Beta v.2.0
    Глобальное обновление проекта

--- js/es6-main.js
+++ js/es6-main.js
@@ -1,109 +1,266 @@
+
+/* WOW JS */
+
+wow = new WOW(
+{
+boxClass:     'wow',      // default
+animateClass: 'animated', // default offset:       200,          // not default
+mobile:       true,       // default
+live:         true        // default
+});
+  
+if (document.body.clientWidth < 400) wow.config.offset = 100;
+  
+wow.init();
+
+/* Smooth Scroll */
+  
+
+$("[data-scroll]").on("click", function(event) {
+  
+  event.preventDefault();
+  
+  let $this = $(this);
+  let blockId = $this.data("scroll");
+  let blockOffset = $(blockId).offset().top + parseInt($(blockId).css("padding-top"));
+  
+  
+    $("html, body").animate({
+       scrollTop: blockOffset
+    }, 500);
+});
+
+
+
+
+/* DATE */
+let date = new Date();
+document.querySelector('.date__day').textContent = date.getDate();
+document.querySelector('.date__month').textContent = date.getMonth() + 1;
+document.querySelector('.date__year').textContent = date.getFullYear();
+
+
+/* HEADER */
+let header = document.getElementById('header');
+
+
 $(function(){
 
     $('.header__slider').slick({
+        asNavFor: '.slider-dots',
         infinite: true,
-        fade: true,
-        prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt=""></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right arrow-slider" src="./images/svg/arrow-right.svg" alt=""></img>',
-        
-        
-        asNavFor: '.slider-dotshead',
-        
+        fade: true, 
+        prevArrow: '<img class="slider-arrow slider-arrow-left" src="./images/svg/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrow slider-arrow-right" src="./images/svg/arrow-right.svg" alt=""></img>', 
     });
 
-   $('.slider-dotshead').slick({
+    $('.slider-dots').slick({  
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      asNavFor: '.header__slider',
+      focusOnSelect: true,   
+    });
+
+
+    $('.slider-arrow-bottom').on('click', function() {
+        $('.header__slider').slick('slickNext');
+    });
+
+    let northLine = document.getElementById('north-line');
+    let westLine = document.getElementById('west-line');
+    let eastLine = document.getElementById('east-line');
+    let southLine = document.getElementById('south-line');
+
+    let linesArr = [northLine, westLine, eastLine, southLine];
+
+    let animateClass = 'map__line--animation';
+
+
+    $('.header__slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        let beforeLine = linesArr[currentSlide];
+        let currentLine = linesArr[nextSlide];
+
+        currentLine.classList.add(animateClass);
+        beforeLine.classList.remove(animateClass);
+    });
+});
+
+let mapSvg = document.querySelector('.header__map-svg');
+let northText = document.querySelector('.north-shore__text');
+
+function changeMap() {
+    if (document.body.clientWidth < 800) {
+        mapSvg.setAttribute("viewBox", "0 40 860 500");
+    }
+
+    if (document.body.clientWidth < 400) {
+        mapSvg.setAttribute("viewBox", "0 40 770 500");
+    }
+
+    if (document.body.clientWidth < 640) {
+        northText.setAttribute("x", "382")
+        northText.setAttribute("y", "260")
+    }  
+}
+
+changeMap(mapSvg, northText);
+
+window.addEventListener('resize', event => {
+  setTimeout(changeMap, 1000);
+  setTimeout(calcPoints, 1000);
+});
+
+/* header/ menu toggle */
+let menuToggle = document.getElementById('menuToggle');
+let aside = header.querySelector('.aside');
+let map = header.querySelector('.map');
+let headerContent = header.querySelector('.header__content');
+
+menuToggle.addEventListener('click', function() {
+    aside.classList.toggle('aside-active');
+    map.classList.toggle('opacity-off');
+    headerContent.classList.toggle('opacity-off');
+});
+
+/* SURF */
+
+let surfMapList = document.getElementById('surf-map-list');
+let pointsList = document.querySelectorAll('.surf-map__item');
+
+function calcPoints() {
+  surfMapList.style.height = surfMapList.offsetWidth / 1.8897 + 'px';
+
+  for (point of pointsList) {
+    point.style.top = surfMapList.offsetHeight / point.dataset.top + 'px';
+    point.style.left = surfMapList.offsetWidth / point.dataset.left + 'px';
+  }
+}
+calcPoints();
+/* SURF / SURF-SLIDER start */
+
+$('.surf-list').slick({
+
+    touchThreshold: 20,
+    rows: 0,
+    touchThreshold: 10,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    prevArrow: '<img class="slider-arrow slider-arrow-left surf-arrow" src="images/svg/arrow-left.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrow slider-arrow-right surf-arrow" src="images/svg/arrow-right.svg" alt=""></img>',
+      responsive: [
+    {
+      breakpoint: 1760,
+      settings: {
         slidesToShow: 4,
-        slidesToScroll: 4,
-        asNavFor: '.slider-dotshead',
-        
-   });
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 1410,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 1079,
+      settings: {
+        slidesToShow: 2.5,
+        slidesToScroll: 1.25
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 723,
+      settings: {
+        slidesToShow: 1.8,
+        slidesToScroll: 0.9
+      }
+    },
+    {
+      breakpoint: 638,
+      settings: {
+        touchThreshold: 30,
+        slidesToShow: 1.4,
+        slidesToScroll: 0.9
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        touchThreshold: 30,
+        slidesToShow: 1.05,
+        slidesToScroll: 0.92,
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        touchThreshold: 30,
+        slidesToShow: 1.05,
+        slidesToScroll: 0.98,
+      }
+    }
+  ]
+});
+
+/* SURF / SURF-SLIDER end */
 
 
-   $('.serf-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt=""></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/svg/arrow-right.svg" alt=""></img>',
-        
-   });
-
-   $('.slider-map').slick({
-     
-     slidesToShow: 8,
-     
-     arrows: false,
-     asNavFor: '.serf-slider',
-     focusOnSelect: true,
-     
-        
-        
-   });
-
-   $('.holder__slider, .shop__slider').slick({
-     infinite: true,
-     fade: true,
-     prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt=""></img>',
-     nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/svg/arrow-right.svg" alt=""></img>',
-     
-    });
-
-    jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/svg/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="images/svg/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
-    jQuery('.quantity').each(function() {
-      var spinner = jQuery(this),
-        input = spinner.find('input[type="number"]'),
-        btnUp = spinner.find('.quantity-up'),
-        btnDown = spinner.find('.quantity-down'),
-        min = input.attr('min'),
-        max = input.attr('max');
-
-      btnUp.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue >= max) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue + 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
-
-      btnDown.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue <= min) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue - 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
-
-    });
 
 
-    $('.quantity-button').on('click', function(){
-      var parents = $(this).parents('.holder-slider__info');
-       let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
-       $('.summ', parents).html('$' + summ);
-      });
-      
-    // здесь рассчитывает сумму с исходными значениями !
-      $('.quantity').each(function() {
-      var parents = $(this).parents('.holder-slider__info');
-      let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
-       $('.summ', parents).html('$' + summ);
-      });
+/* Sleep  */
 
-      $('.surfboard-box__circle').on('click', function(){
-        $(this).toggleClass('active')
-      });
+$(document).ready(function() {
+  let sleepPrice = 0;
+  let sleepRate = 17.45;
 
-    //$('.quantity-button').on('click', function(){
-     // let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() -1) * $('.summ').data('guests');
-    //  $('.summ').html('$' + summ);
-   // });
+  function setPrice() {
+    sleepPrice = +$('.sleep-info__nights').val() * +$('.sleep-info__guests').val() * sleepRate;
+    $('.sleep-info__price').html(sleepPrice.toFixed(2) + ' USD');
+    return;
+  }
+  setPrice()
 
-   // let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() -1) * $('.summ').data('guests');
-  //  $('.summ').html('$' + summ);
+	$('.minus').click( function () {
+		let $input = $(this).parent().siblings('input');
+		let count = parseInt($input.val()) - 1;
+		count = count < 1 ? 1 : count;
+		$input.val(count);
+		$input.change();
+
+    setPrice()
+		return false;
+	});
+	$('.plus').click( function () {
+		let $input = $(this).parent().siblings('input');
+    if ($input.val() >= 9) return false; 
+		$input.val(parseInt($input.val()) + 1);
+		$input.change();
+
+    setPrice()
+		return false;
+	});
 
 });
+
+/* Shop  */
+
+let surfboardPoints = document.querySelectorAll('.surfboard__circle');
+
+for (point of surfboardPoints) {
+    let surfboardStrip = point.querySelector('.surfboard__strip:last-child');
+    let surfboardContent = point.nextElementSibling;
+
+    point.addEventListener("click", function() {
+        surfboardStrip.classList.toggle('active');
+        surfboardContent.classList.toggle('active');
+    });
+}
